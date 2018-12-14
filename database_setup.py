53e979bf83704262df2ef2355942bfbe266ff4e2
khalid mohammed <34813370+ikhdev@users.noreply.github.com>
2018-12-14 22:07:39 +0300

    Add files via upload

---
+++ database_setup.py
@@ -0,0 +1,63 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref, sessionmaker
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User, backref="category")
+    item1 = relationship("CatalogItem",
+                         cascade="all,delete", backref="category2")
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class CatalogItem(Base):
+    __tablename__ = 'catalog_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category,
+                            backref=backref("item", cascade="all,delete"))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User, backref="catalog_item")
+
+    @property
+    def serialize(self):
+        return{
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price
+        }
+
+
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
+
+Base.metadata.create_all(engine)
